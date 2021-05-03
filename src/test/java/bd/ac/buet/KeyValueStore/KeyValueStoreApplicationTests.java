5f4503cbdafa7f02674744e9b0838b315a2de0d0
monjurmorshed793 <monjurmorshed793@gmail.com>
2021-05-03 13:58:48 +0600

    initial project set up

---
+++ src/test/java/bd/ac/buet/KeyValueStore/KeyValueStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.ac.buet.KeyValueStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
