9e01ff04d6f3f4df100c8edb3ec9e1bf2292b589
monjurmorshed793 <monjurmorshed793@gmail.com>
2021-05-12 14:03:34 +0600

    non-reactive kafka configured

--- src/main/java/bd/ac/buet/KeyValueStore/service/MessageProcessor.java
+++ src/main/java/bd/ac/buet/KeyValueStore/service/MessageProcessor.java
@@ -1,9 +1,38 @@
 package bd.ac.buet.KeyValueStore.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Slf4j
 public class MessageProcessor {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public MessageProcessor(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message){
+        kafkaTemplate.send(topic, message);
+    }
+
+    @Async
+    public void testSendMessage() {
+        for(int i=0; i<100; i++){
+            sendMessage("paxos-topic", "hellos "+i);
+            //Thread.sleep(5000);
+        }
+    }
+
+    @KafkaListener(topics = "paxos-topic")
+    public void listenToPaxosTopic(String message){
+      log.info("in the listener");
+      log.info(message);
+    }
 
 }
