08d3001748067d90728744a5cb70270c114e92f6
Munna <monjurmorshed793@gmail.com>
2021-05-15 10:21:26 +0600

    Redis embedded integration tests added

--- src/main/java/bd/ac/buet/KeyValueStore/configuration/RedisConfig.java
+++ src/main/java/bd/ac/buet/KeyValueStore/configuration/RedisConfig.java
@@ -3,21 +3,24 @@ package bd.ac.buet.KeyValueStore.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
     @Bean
-    JedisConnectionFactory jedisConnectionFactory(){
-        return new JedisConnectionFactory();
+    LettuceConnectionFactory jedisConnectionFactory(RedisProperties redisProperties){
+        return new LettuceConnectionFactory(redisProperties.getRedisHost(), redisProperties.getRedisPort());
     }
 
     @Bean
-    public RedisTemplate<Long, Object> redisTemplate(){
+    public RedisTemplate<Long, Object> redisTemplate(LettuceConnectionFactory connectionFactory){
         final RedisTemplate<Long, Object> template =
                 new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return template;
     }
---
+++ src/test/java/bd/ac/buet/KeyValueStore/repository/ServerInfoRepositoryTest.java
@@ -0,0 +1,35 @@
+package bd.ac.buet.KeyValueStore.repository;
+
+import bd.ac.buet.KeyValueStore.configuration.TestRedisConfiguration;
+import bd.ac.buet.KeyValueStore.model.ServerInfo;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.shaded.org.apache.commons.lang.math.RandomUtils;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestRedisConfiguration.class)
+@ExtendWith(SpringExtension.class)
+public class ServerInfoRepositoryTest {
+    @Autowired
+    private ServerInfoRepository serverInfoRepository;
+
+    @Test
+    public void shouldSaveServerInfo(){
+        ServerInfo serverInfo = new ServerInfo();
+        serverInfo.setId(1L);
+        serverInfo.setName("server1");
+        serverInfo.setUpdatedOn(Instant.now());
+        serverInfo = serverInfoRepository.save(serverInfo);
+        assertNotNull(serverInfo);
+    }
+}
