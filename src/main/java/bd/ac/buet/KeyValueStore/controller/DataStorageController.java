3ef017b31c28d6ae4da7e2ff761d576e8d1014a5
Munna <monjurmorshed793@gmail.com>
2021-05-20 00:41:17 +0600

    Controller updated.

--- src/main/java/bd/ac/buet/KeyValueStore/controller/DataStorageController.java
+++ src/main/java/bd/ac/buet/KeyValueStore/controller/DataStorageController.java
@@ -1,22 +1,31 @@
 package bd.ac.buet.KeyValueStore.controller;
 
 import bd.ac.buet.KeyValueStore.model.ObjectStore;
+import bd.ac.buet.KeyValueStore.repository.ObjectStoreRepository;
 import bd.ac.buet.KeyValueStore.service.paxos.ProposerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
 public class DataStorageController {
 
     private final ProposerService proposerService;
+    private final ObjectStoreRepository objectStoreRepository;
 
-    public DataStorageController(ProposerService proposerService) {
+    public DataStorageController(ProposerService proposerService, ObjectStoreRepository objectStoreRepository) {
         this.proposerService = proposerService;
+        this.objectStoreRepository = objectStoreRepository;
     }
 
-    @PutMapping
+    @PutMapping("/")
     public  ObjectStore storeOrUpdateData(ObjectStore objectStore){
         return proposerService.propose(objectStore);
+    }
+
+    @GetMapping ("/id/{id}")
+    public ObjectStore getData( @PathVariable String id){
+        return objectStoreRepository.findById(id).get();
     }
 }
